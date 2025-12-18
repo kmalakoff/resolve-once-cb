506e9fc4bba6c34391ee5680ec5e059445708051
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-17 20:59:06 -0800

    Migrate to void

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ const RESOLVING = 1;
 const RESOLVED_SUCCESS = 2;
 const RESOLVED_ERROR = 3;
 
-export type Callback<T> = (error?: Error, result?: T) => undefined;
-export type Resolver<T> = (callback: Callback<T>) => undefined;
+export type Callback<T> = (error?: Error, result?: T) => void;
+export type Resolver<T> = (callback: Callback<T>) => void;
 
 export default function resolveOnce<T>(fn: Resolver<T>): Resolver<T> {
   let state = UNRESOLVED;
@@ -15,7 +15,7 @@ export default function resolveOnce<T>(fn: Resolver<T>): Resolver<T> {
     if (state === RESOLVING) return;
     state = RESOLVING;
 
-    function callback(error?: Error, value?: T): undefined {
+    function callback(error?: Error, value?: T): void {
       if (state !== RESOLVING) return;
       if (error) {
         state = RESOLVED_ERROR;
@@ -35,7 +35,7 @@ export default function resolveOnce<T>(fn: Resolver<T>): Resolver<T> {
     }
   }
 
-  return (callback: Callback<T>): undefined => {
+  return (callback: Callback<T>): void => {
     if (typeof callback !== 'function') throw new Error('resolve-once-cb missing callback');
     if (state === RESOLVED_SUCCESS) return callback(null, result as T);
     if (state === RESOLVED_ERROR) return callback(result as Error);
